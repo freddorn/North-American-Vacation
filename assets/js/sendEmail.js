fe9969261bb6685acaf9b526e3e63d2b56f23ad9
Ubuntu <ubuntu@ip-172-31-90-44.ec2.internal>
2019-07-01 21:08:31 +0000

    experimented with email form

--- assets/js/sendEmail.js
+++ assets/js/sendEmail.js
@@ -1,16 +1,20 @@
-function sendMail(contactForm) {
-    emailjs.send("gmail", "rosie", {
-            "from_name": contactForm.name.value,
-            "from_email": contactForm.emailaddress.value,
-            "message": contactForm.projectsummary.value
-        })
-        .then(
-            function(response) {
-                console.log("SUCCESS", response);
-            },
-            function(error) {
-                console.log("FAILED", error);
-            }
-        );
-    return false; // To block from loading a new page
-}
+
+var myform = $("form#myform");
+myform.submit(function(event){
+	event.preventDefault();
+
+  // Change to your service ID, or keep using the default service
+  var service_id = "default_service";
+  var template_id = "vacation";
+
+  myform.find("button").text("Sending...");
+  emailjs.sendForm(service_id,template_id,myform[0])
+  	.then(function(){ 
+    	alert("Sent!");
+       myform.find("button").text("Send");
+    }, function(err) {
+       alert("Send email failed!\r\n Response:\n " + JSON.stringify(err));
+       myform.find("button").text("Send");
+    });
+  return false;
+});
